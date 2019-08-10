5815b154c703cb69673a07375123b101bdd94220
TarCV <TarCV@users.noreply.github.com>
2019-08-10 07:21:42 +0300

    Implement custom argument support in stand-alone runner

--- tests/app/functionalTestCustom.sh
+++ tests/app/functionalTestCustom.sh
@@ -28,3 +28,34 @@ if [[ ${CI_STUBBED} != 'true' ]]; then
 fi
 ./gradlew :app:tongsF2DebugAndroidTest --stacktrace
 ./gradlew :app:testF2DebugUnitTest
+
+rm ./*.log || true
+rm -r app/build/reports || true
+rm app/tongs.json || true
+
+if [[ ${CI_STUBBED} != 'true' ]]; then
+    ./gradlew :app:uninstallF1Debug :app:uninstallF1DebugAndroidTest
+    ./gradlew :app:uninstallF2Debug :app:uninstallF2DebugAndroidTest --stacktrace
+fi
+
+rm ./*.log || true
+rm -r app/build/reports || true
+rm app/tongs.json || true
+
+cp app/tongs.sample.json app/tongs.json
+TEST_ROOT="$(pwd)"
+TEST_ROOT=$(echo "$TEST_ROOT" | perl -lape 's/^\/([a-z])\//$1:\//g')
+SDK_PATH=$(echo "$ANDROID_HOME" | sed 's#\\#/#' | perl -lape 's/^\/([a-z])\//$1:\//g')
+sed -i.bak "s/DEVICE1/${DEVICE1}/" app/tongs.json
+sed -i.bak "s/DEVICE2/${DEVICE2}/" app/tongs.json
+sed -i.bak "s#TEST_ROOT#${TEST_ROOT}#" app/tongs.json
+if [[ ${CI_STUBBED} != 'true' ]]; then
+    sed -i.bak "s/TEST_RUN_TYPE/RECORD_LISTENER_EVENTS/" app/tongs.json
+else
+    sed -i.bak "s/TEST_RUN_TYPE/STUB_PARALLEL_TESTRUN/" app/tongs.json
+fi
+
+pushd ../../plugin
+    ./gradlew :tongs-runner:run --stacktrace -PworkingDir="$TEST_ROOT" -Pargs="--sdk $SDK_PATH --apk $TEST_ROOT/app/build/outputs/apk/f2/debug/app-f2-universal-debug.apk --test-apk $TEST_ROOT/app/build/outputs/apk/androidTest/f2/debug/app-f2-debug-androidTest.apk --config $TEST_ROOT/app/tongs.json"
+popd
+./gradlew :app:testF2DebugUnitTest
