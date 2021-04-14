55d46e08ec74927811b876ee0a2f160f0b2967d2
TarCV <TarCV@users.noreply.github.com>
2021-04-14 03:59:53 +0300

    Make annotation package prefix configurable

--- plugin/tongs-plugin-android/src/main/java/com/github/tarcv/tongs/plugin/android/PropertiesRule.kt
+++ plugin/tongs-plugin-android/src/main/java/com/github/tarcv/tongs/plugin/android/PropertiesRule.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 TarCV
+ * Copyright 2021 TarCV
  * Copyright 2016 Shazam Entertainment Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
@@ -8,32 +8,37 @@
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
-
 package com.github.tarcv.tongs.plugin.android
 
+import com.github.tarcv.tongs.api.HasConfiguration
 import com.github.tarcv.tongs.api.run.TestCaseEvent
 import com.github.tarcv.tongs.api.testcases.TestCase
 import com.github.tarcv.tongs.api.testcases.TestCaseRule
 import com.github.tarcv.tongs.api.testcases.TestCaseRuleContext
 import com.github.tarcv.tongs.api.testcases.TestCaseRuleFactory
 
-class PropertiesTestCaseRuleFactory: TestCaseRuleFactory<PropertiesTestCaseRule> {
+class PropertiesTestCaseRuleFactory
+    : TestCaseRuleFactory<PropertiesTestCaseRule>, HasConfiguration {
+    override val configurationSections: Array<String> = arrayOf("propertyAnnotations")
+
     override fun testCaseRules(context: TestCaseRuleContext): Array<out PropertiesTestCaseRule> {
-        return arrayOf(PropertiesTestCaseRule())
+        return arrayOf(PropertiesTestCaseRule(context.configuration.pluginConfiguration["package"] as? String))
     }
 }
 
-class PropertiesTestCaseRule: TestCaseRule {
+class PropertiesTestCaseRule(packagePrefix: String?) : TestCaseRule {
+    private val packagePrefix = packagePrefix ?: "com.github.tarcv.tongs"
+
     override fun transform(testCaseEvent: TestCaseEvent): TestCaseEvent {
         val properties = HashMap<String, String>(testCaseEvent.testCase.properties)
         testCaseEvent.testCase.annotations.forEach {
             when (it.fullyQualifiedName) {
-                "com.github.tarcv.tongs.TestProperties" -> {
+                "$packagePrefix.TestProperties" -> {
                     val keys = it.properties["keys"] as List<String>
                     val values = it.properties["values"] as List<String>
                     keyValueArraysToProperties(properties, keys, values)
                 }
-                "com.github.tarcv.tongs.TestPropertyPairs" -> {
+                "$packagePrefix.TestPropertyPairs" -> {
                     val values = it.properties["value"] as List<String>
                     keyValuePairsToProperties(properties, values)
                 }
