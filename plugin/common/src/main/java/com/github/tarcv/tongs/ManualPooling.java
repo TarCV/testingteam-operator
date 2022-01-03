ac94c80c9c2c58f270081ee9718b4fabf7d5747a
TarCV <TarCV@users.noreply.github.com>
2022-01-03 10:03:16 +0300

    Fix module names

--- plugin/common/src/main/java/com/github/tarcv/tongs/ManualPooling.java
+++ plugin/common/src/main/java/com/github/tarcv/tongs/ManualPooling.java
@@ -1,6 +1,6 @@
 /*
- * Copyright 2019 TarCV
- * Copyright 2015 Shazam Entertainment Limited
+ * Copyright 2020 TarCV
+ * Copyright 2016 Shazam Entertainment Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
  *
@@ -9,10 +9,12 @@
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
-include ':common'
-include ':common-test'
-include ':runner'
-include ':gradle-plugin'
-include ':plugin-android-ondevice'
-include ':plugin-api'
-include ':plugin-android'
+package com.github.tarcv.tongs;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class ManualPooling {
+
+    public Map<String, Collection<String>> groupings;
+}
